8d4f1e1055be3eaaa835ce6457ac0213a3c650e2
YaronP <yaroon15@gmail.com>
2023-02-10 21:09:19 +0200

    add to localStorage

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -2,21 +2,38 @@ import { useReducer, useContext, createContext } from "react";
 import axios from "axios";
 import actions from "./actions";
 import reducer from "./reducer";
+
+const user = localStorage.getItem("user");
+const token = localStorage.getItem("token");
+const userLocation = localStorage.getItem("location");
+
 const initialState = {
 	isLoading: false,
 	showAlert: false,
 	alertText: "",
 	alertType: "",
-	user: null,
-	token: null,
-	userLocation: "",
-	jobLocation: "",
+	user: user ? JSON.parse(user) : null,
+	token,
+	userLocation: userLocation || "",
+	jobLocation: userLocation || "",
 };
 
 const AppContext = createContext();
 const apiV1 = "/api/v1";
 const AppProvider = ({ children }) => {
 	const [state, dispatch] = useReducer(reducer, initialState);
+
+	const addUserToLocalStorage = ({ user, token, location }) => {
+		localStorage.setItem("user", JSON.stringify(user));
+		localStorage.setItem("token", token);
+		localStorage.setItem("location", location);
+	};
+
+	const removeUserToLocalStorage = ({ user, token, location }) => {
+		localStorage.removeItem("user");
+		localStorage.removeItem("token");
+		localStorage.removeItem("location");
+	};
 
 	const displayAlert = () => {
 		dispatch({ type: actions.DISPLAY_ALERT });
@@ -39,6 +56,7 @@ const AppProvider = ({ children }) => {
 				type: actions.REGISTER_USER_SUCCESS,
 				payload: { user, token, location },
 			});
+			addUserToLocalStorage({ user, token, location });
 		} catch (error) {
 			// console.log(error.response);
 			dispatch({
@@ -46,7 +64,6 @@ const AppProvider = ({ children }) => {
 				payload: { msg: error.response.data.msg },
 			});
 		}
-
 		clearAlert();
 	};
 
