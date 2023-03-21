06f7aec7f50cfbb9bf4601c66b92ed3a0767b25e
YaronP <yaroon15@gmail.com>
2023-03-21 19:14:35 +0200

    added token auth middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,11 @@
+import { UnauthenticatedError } from "../errors/index.js";
+
+const auth = async (req, res, next) => {
+	const authHeader = req.headers.authorization;
+	if (!authHeader) {
+		throw new UnauthenticatedError("Authentication Invalid");
+	}
+	next();
+};
+
+export default auth;
